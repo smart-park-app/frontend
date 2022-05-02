e2fe1783cc4c17c190db6e6b8058780ae3339c55
Tarun Wadhwa <tarunwadhwa13@gmail.com>
2022-05-02 12:14:23 +0530

    Updated bottom sheet

--- lib/src/catalog/catalog_view.dart
+++ lib/src/catalog/catalog_view.dart
@@ -1,8 +1,12 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:mapmyindia_gl/mapmyindia_gl.dart';
 import 'package:smart_park/src/widgets/app_drawer.dart';
 import 'catalog_controller.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:mapmyindia_place_widget/mapmyindia_place_widget.dart';
+import 'package:geocoding/geocoding.dart';
 
 /// Displays available slots near a location
 ///
@@ -29,6 +33,8 @@ class CatalogView extends StatelessWidget {
     late MapmyIndiaMapController mapController;
     late Symbol symbol;
     var userLocationLatLng = LatLng(30.7410517, 76.779015);
+    late ELocation? eLocation;
+    String selectedAddr = "";
 
     return Scaffold(
         drawer: const AppDrawer(),
@@ -40,7 +46,7 @@ class CatalogView extends StatelessWidget {
                 .then((value) => {
                       userLocationLatLng =
                           LatLng(value.latitude, value.longitude),
-                      mapController.moveCamera(CameraUpdate.newLatLngZoom(
+                      mapController.easeCamera(CameraUpdate.newLatLngZoom(
                           LatLng(userLocationLatLng.latitude,
                               userLocationLatLng.longitude),
                           15))
@@ -59,36 +65,15 @@ class CatalogView extends StatelessWidget {
               ),
               myLocationEnabled: true,
               myLocationTrackingMode: MyLocationTrackingMode.TrackingCompass,
-              myLocationRenderMode: MyLocationRenderMode.COMPASS,
+              myLocationRenderMode: MyLocationRenderMode.NORMAL,
               onUserLocationUpdated: (location) => {
-                mapController.moveCamera(CameraUpdate.newLatLngZoom(
+                mapController.easeCamera(CameraUpdate.newLatLngZoom(
                     LatLng(location.position.latitude,
                         location.position.longitude),
                     15))
               },
-              onMapCreated: (map) => {
+              onMapCreated: (map) async => {
                 mapController = map,
-                // showModalBottomSheet<void>(
-                //     shape: RoundedRectangleBorder(
-                //         borderRadius:
-                //             BorderRadius.vertical(top: Radius.circular(20.0))),
-                //     isScrollControlled: true,
-                //     isDismissible: false,
-                //     context: context,
-                //     builder: (BuildContext context) {
-                //       return SingleChildScrollView(
-                //           child: Container(
-                //         height: 350,
-                //         color: Colors.grey,
-                //         child: Column(
-                //           mainAxisAlignment: MainAxisAlignment.center,
-                //           mainAxisSize: MainAxisSize.min,
-                //           children: <Widget>[
-                //             const Text('Available Slots'),
-                //           ],
-                //         ),
-                //       ));
-                //     })
               },
               onMapClick: (point, latlng) async => {
                 mapController.removeSymbol(symbol),
@@ -100,22 +85,91 @@ class CatalogView extends StatelessWidget {
               top: 10,
               right: 15,
               left: 15,
-              child: Container(
-                color: Colors.white,
+              child: ElevatedButton(
+                onPressed: () => {
+                  openPlaceAutocomplete(PlaceOptions(
+                          enableTextSearch: true,
+                          saveHistory: true,
+                          hint: "Search for Location near your destination"))
+                      .then((value) => {
+                            eLocation = value.eLocation,
+                            print("===========> Elocation -------" +
+                                json.encode(eLocation?.toJson())),
+                            selectedAddr =
+                                eLocation?.placeName.toString() ?? "",
+                            selectedAddr = selectedAddr +
+                                " " +
+                                (eLocation?.placeAddress.toString() ?? ""),
+                            if (selectedAddr != "")
+                              {
+                                print(
+                                    "========== Searching coordinates for - " +
+                                        selectedAddr.trim()),
+                                locationFromAddress(selectedAddr.trim()).then(
+                                    (response) {
+                                  if (response.isNotEmpty) {
+                                    print("====== Response =========" +
+                                        json.encode(response.first.toJson()));
+                                    mapController.moveCamera(
+                                        CameraUpdate.newLatLngZoom(
+                                            LatLng(response.first.latitude,
+                                                response.first.longitude),
+                                            15));
+                                    mapController
+                                        .addSymbol(SymbolOptions(
+                                            geometry: LatLng(
+                                                response.first.latitude,
+                                                response.first.longitude)))
+                                        .then((value) => symbol = value);
+                                    // showModalBottomSheet(
+                                    //     isScrollControlled: true,
+                                    //     context: context,
+                                    //     builder: (BuildContext context) {
+                                    //       return Card(
+                                    //           elevation: 12.0,
+                                    //           shape: RoundedRectangleBorder(
+                                    //               borderRadius:
+                                    //                   BorderRadius.circular(
+                                    //                       24)),
+                                    //           margin: const EdgeInsets.all(0),
+                                    //           child: Container(
+                                    //             height: 350,
+                                    //             color: Colors.grey,
+                                    //             child: Column(
+                                    //               children: <Widget>[
+                                    //                 CustomDraggingHandle(),
+                                    //                 const Text(
+                                    //                   'Available Slots',
+                                    //                   textAlign:
+                                    //                       TextAlign.center,
+                                    //                 ),
+                                    //               ],
+                                    //             ),
+                                    //           ));
+                                    //     });
+                                  } else {
+                                    print("Error response seems to be empty");
+                                  }
+                                }, onError: (e) {
+                                  print("Error geocoding -----> " + e.code);
+                                })
+                              }
+                            else
+                              {
+                                print("The selected place is not valid ----> " +
+                                    selectedAddr.toString())
+                              }
+                          }),
+                },
+                style: ElevatedButton.styleFrom(
+                    primary: Colors.white,
+                    onPrimary: Colors.black,
+                    textStyle: const TextStyle(
+                        color: Colors.black, fontWeight: FontWeight.bold)),
                 child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
-                    Expanded(
-                      child: TextField(
-                        cursorColor: Colors.black,
-                        keyboardType: TextInputType.text,
-                        textInputAction: TextInputAction.go,
-                        decoration: InputDecoration(
-                            border: InputBorder.none,
-                            contentPadding:
-                                EdgeInsets.symmetric(horizontal: 15),
-                            hintText: "Search landmark"),
-                      ),
-                    ),
+                    const Text("Search for a landmark"),
                     IconButton(
                       splashColor: Colors.grey,
                       icon: const Icon(Icons.search),
@@ -125,7 +179,70 @@ class CatalogView extends StatelessWidget {
                 ),
               ),
             ),
+            DraggableScrollableSheet(
+              initialChildSize: 0.08,
+              minChildSize: 0.08,
+              maxChildSize: 0.5,
+              builder:
+                  (BuildContext context, ScrollController scrollController) {
+                return SingleChildScrollView(
+                  controller: scrollController,
+                  child: CustomScrollViewContent(),
+                );
+              },
+            ),
           ],
         ));
+  }
+}
+
+class CustomDraggingHandle extends StatelessWidget {
+  const CustomDraggingHandle({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 5,
+      width: 30,
+      decoration: BoxDecoration(
+          color: Colors.grey[200], borderRadius: BorderRadius.circular(16)),
+    );
+  }
+}
+
+/// Content of the DraggableBottomSheet's child SingleChildScrollView
+class CustomScrollViewContent extends StatelessWidget {
+  const CustomScrollViewContent({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 12.0,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(24)),
+      margin: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(24),
+        ),
+        child: CustomInnerContent(),
+      ),
+    );
+  }
+}
+
+class CustomInnerContent extends StatelessWidget {
+  const CustomInnerContent({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        SizedBox(height: 12),
+        CustomDraggingHandle(),
+        SizedBox(height: 16),
+        Text("Search to see available slots"),
+        SizedBox(height: 500),
+      ],
+    );
   }
 }
